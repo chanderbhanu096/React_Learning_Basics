90fb208b2dfd4e988f9c04d820d82eced27bebfe
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-07 14:08:55 +0100

    destructuring

--- Ch04/04_01/start/react-app/src/App.js
+++ Ch04/04_01/start/react-app/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+// const cities = ["Tokyo","London", "Paris", "New York"];
+// console.log(cities[0]);
+// OR 
+const [first,second, third] = ["Tokyo","London", "Paris", "New York"];
+console.log(first);
+console.log(second);
+console.log(third);
+
+function App({library}) {
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {library}</h1>
     </div>
   );
 }
--- Ch04/04_01/start/react-app/src/index.js
+++ Ch04/04_01/start/react-app/src/index.js
@@ -8,9 +8,9 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
 
   // StrictMode is a tool for highlighting potential problems in an application.
-  // <React.StrictMode>
-  //   <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App library = "React" />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
