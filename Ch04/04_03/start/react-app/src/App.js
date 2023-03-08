8b1ec18d4f86096eeb45e0a8627c3b6f14ecc157
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-08 21:09:15 +0100

    use state

--- Ch04/04_03/start/react-app/src/App.js
+++ Ch04/04_03/start/react-app/src/App.js
@@ -1,9 +1,13 @@
 import "./App.css";
+import { useState } from "react";
 
-function App() {
+function App({propvariable}) {
+  const what = useState("hello");
+  const [emotion, setEmotion] = useState("happy");
+  console.log(what);
   return (
     <div className="App">
-      <h1>Hello from React</h1>
+      <h1>Hello from {propvariable}</h1>
     </div>
   );
 }
