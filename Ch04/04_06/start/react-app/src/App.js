b8d8a8bef12c4c629fd6a84d6352f8ac1c0024a6
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-08 21:35:16 +0100

    unecessary files

--- Ch04/04_06/start/react-app/src/App.js
+++ Ch04/04_06/start/react-app/src/App.js
@@ -1,12 +1,21 @@
 import "./App.css";
 import { useState, useEffect } from "react";
+// useEffect is a hook that allows you to run code when the state of a component changes  (or when the component mounts or unmounts)
+// useState is a hook that allows you to create a state variable in a functional component
 
 function App() {
   const [emotion, setEmotion] = useState("happy");
+  const [secondary, setSecondary] = useState("tired");
 
   useEffect(() => {
     console.log(`It's ${emotion} around here!`);
   }, [emotion]);
+  // this is also possible you can have multiple useEffects in a component
+  //   useEffect(() => { console.log(`It's ${emotion} around here!`); }, [emotion, secondary]);
+  
+  useEffect(() => {
+    console.log(`It's ${secondary} around here!`);
+  }, [secondary]);
 
   return (
     <div className="App">
@@ -19,6 +28,11 @@ function App() {
       >
         Excited
       </button>
+     <h1>Current secondary emotion is {secondary}</h1>
+      <button onClick={() => setSecondary ("sleepy")}>
+        Sleepy
+      </button>
+
     </div>
   );
 }
