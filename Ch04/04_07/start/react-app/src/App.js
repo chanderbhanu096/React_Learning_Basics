97bdd171a300bccf541d22861c4d47815a759622
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-09 20:52:59 +0100

    useReducer

--- Ch04/04_07/start/react-app/src/App.js
+++ Ch04/04_07/start/react-app/src/App.js
@@ -1,13 +1,19 @@
 import "./App.css";
-import { useState, useEffect } from "react";
+import { useState, useReducer } from "react";
+
 
 function App() {
-  const[checked, setChecked] = useState(false);
+  // const[checked, setChecked] = useState(false);
+  const[checked, setChecked] = useReducer((checked) => !checked,false);
+
 
   return (
     <div className="App">
-      <input type = "checkbox"  value={checked} 
-      onChange={() => setChecked((checked) => !checked)} /> 
+      <input type = "checkbox"  
+      value={checked} 
+      // onChange is a special prop that is used to handle changes to the input element
+      // ((checked) => !checked)} is changing the state of the checked variable from true to false or false to true
+      onChange={ setChecked} /> 
       <label>{checked ? "checked":"unchecked"}</label>
     </div>) } 
 export default App;
