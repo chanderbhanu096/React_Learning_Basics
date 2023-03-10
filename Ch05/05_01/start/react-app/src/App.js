68dbe056aeaa2ebfcfbd1ad1675b8ceda45bd3b2
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-10 20:50:19 +0100

    custom hook

--- Ch05/05_01/start/react-app/src/App.js
+++ Ch05/05_01/start/react-app/src/App.js
@@ -2,26 +2,46 @@ import "./App.css";
 import { useState } from "react";
 // useref is a hook that allows you to create a reference to a DOM element or a form element
 
+// custom hook
+// custom hooks are functions that start with use
+// custom hooks are used to share logic between components
+function useInputValue(defaultValue = "") {
+  const [value, setValue] = 
+    useState(defaultValue);
+    // useState is a custom hook that returns an array with two elements
+    // the first element is the value of the state
+  return [
+    {
+      value,
+      onChange: (event) => setValue(event.target.value),
+    },
+    // second element is a function that allows you to change the value of the state
+    () => setValue(defaultValue),
+  ];
+}
+
+
 function App() {
-  const[title, setTitle] = useState(""); // default title is empty
-  const[color, setColor] = useState("#000000"); // default color is black
+  const[titleProps, resetTitle] = useState(""); // default title is empty
+  const[colorProps, resetColor] = useState("#000000"); // default color is black
   const submit = (e) => {
     e.preventDefault();
-    alert(`${title}, ${color}`);
-    // setTitle("");
-    // setColor("#000000");
+    alert(`${titleProps.value}, ${colorProps.value}`);
+    resetTitle("");
+    resetColor("#000000");
   };
 
   return (
     <form onSubmit={submit}>
       <input
-        value={title}
-        onChange={(event) => setTitle(event.target.value)}
+      // {...titleProps} is a spread operator that is used to pass all the properties of the titleProps object to the input element
+        {...titleProps}
         type="text"
         placeholder="color title..."
       />
-      <input value={color} 
-      onChange={(event) => setColor(event.target.value)}
+      <input 
+      // {...colorProps} is a spread operator that is used to pass all the properties of the colorProps object to the input element
+      {...colorProps}
       type="color" />
       <button>ADD</button>
     </form>
