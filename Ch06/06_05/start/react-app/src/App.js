440d8c08e4c6111e15728904d47965b6c541b7ed
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-12 21:18:27 +0100

    render props

--- Ch06/06_05/start/react-app/src/App.js
+++ Ch06/06_05/start/react-app/src/App.js
@@ -1,65 +1,46 @@
 import "./App.css";
-import { useState, useEffect } from "react";
 
-const query = `
-query {
-  allLifts {
-    name
-    elevationGain
-    status
+const tahao_peaks = [
+  {
+    name: "Tahoe Peak",
+    elevationGain: 2000,
+    status: "open"
+  },
+  {
+    name: "Mt. Rose",
+    elevationGain: 3000,
+    status: "open"
+  },
+  {
+    name: "Mt. Tallac",
+    elevationGain: 4000,
+    status: "open"
   }
-}
-`;
+];
 
-const opts = {
-  method: "POST",
-  headers: { "Content-Type": "application/json" },
-  body: JSON.stringify({ query })
-};
-
-function Lift({ name, elevationGain, status }) {
-  return (
-    <div>
-      <h1>{name}</h1>
-      <p>
-        {elevationGain} {status}
-      </p>
-    </div>
-  );
-}
-
-function App() {
-  const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
-
-  useEffect(() => {
-    setLoading(true);
-    fetch(
-      `https://snowtooth.moonhighway.com/`,
-      opts
-    )
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, []);
-
-  if (loading) return <h1>Loading...</h1>;
-  if (error)
-    return <pre>{JSON.stringify(error)}</pre>;
-  if (!data) return null;
-  return (
-    <div>
-      {data.data.allLifts.map((lift) => (
-        <Lift
-          name={lift.name}
-          elevationGain={lift.elevationGain}
-          status={lift.status}
-        />
+function List({data,renderItem,renderEmpty}){
+  return!data.length?(renderEmpty()
+  ):(
+    <ul>
+      {data.map((item)=>(
+        <li key = {item.name}>
+          {renderItem(item)}
+        </li>
       ))}
-    </div>
+    </ul>
   );
 }
+function App(){
+  return (
+    <List
+      data={tahao_peaks}
+      renderEmpty = {<p>there is no peaks</p>}
+      renderItem = {(item=><>
+        {item.name} - {item.elevationGain} ft.
+        </>
+    )}
+    />
 
+  );
+}
 export default App;
