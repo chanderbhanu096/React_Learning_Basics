c62ca27411a97b2ad84078a857c10c1e9a9871b9
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-13 20:58:59 +0100

    Using Router

--- Ch07/07_01/start/react-app/src/App.js
+++ Ch07/07_01/start/react-app/src/App.js
@@ -1,38 +1,19 @@
 import "./App.css";
 
-const tahoe_peaks = [
-  { name: "Freel", elevation: 10891 },
-  { name: "Monument", elevation: 10067 },
-  { name: "Pyramid", elevation: 9983 },
-  { name: "Tallac", elevation: 9735 }
-];
-
-function List({ data, renderItem, renderEmpty }) {
-  return !data.length ? (
-    renderEmpty
-  ) : (
-    <ul>
-      {data.map((item) => (
-        <li key={item.name}>
-          {renderItem(item)}
-        </li>
-      ))}
-    </ul>
-  );
+function Home() {
+  return <div>My Website</div>
+}
+export function About() {
+  return <div>About</div>
+}
+export function Contact() {
+  return <div>Contact</div>
 }
 
 function App() {
-  return (
-    <List
-      data={tahoe_peaks}
-      renderEmpty={<p>This list is empty</p>}
-      renderItem={(item) => (
-        <>
-          {item.name} - {item.elevation} ft.
-        </>
-      )}
-    />
-  );
+  return <div>
+    <Home />
+  </div>
 }
 
 export default App;
