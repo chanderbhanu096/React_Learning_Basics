818c1325a0399b262e39b992933d9ca879b7c9fd
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-14 21:50:26 +0100

    formatting

--- Ch08/08_01/start/react-app/src/functions.test.js
+++ Ch08/08_01/start/react-app/src/functions.test.js
@@ -1,2 +1,6 @@
 import { timesTwo } from './functions';
-test('multiplies by two', () => { expect(timesTwo(4)).toBe(8); });
+test('multiplies by two', () => {
+    /*  expect() is a function that takes a value, called the "actual" AND
+      compares it to a value called the "expected" in the second argument i.e toBe() */
+        
+     expect(timesTwo(4)).toBe(8); });
