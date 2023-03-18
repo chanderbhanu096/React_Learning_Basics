900653ea80bd6ceaeefb7b8ebcd13be437aa6d42
chanderbhanu096 <chanderbhanu096@gmail.com>
2023-03-18 22:19:47 +0100

    test 2

--- Hooks/useState_Hooks/my-app/src/index.js
+++ Hooks/useState_Hooks/my-app/src/index.js
@@ -1,15 +1,18 @@
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
+// import App from './App';athei
 import reportWebVitals from './reportWebVitals';
 
 function App() {
-  const result = useState();
-  console.log(result);
+  // status is the variable name and useState is the function used to set the value of the variable not delivered default         
+  const [status,setStatus] = useState("Not Delivered");
+
   return (
     <div className="App">
-      <h1>The Package id: </h1>  
+      <h1>The Package id: {status}</h1>  
+      {/* setStatus is the function used to set/change the value of the variable default value "Not Delivered" */}
+      <button onClick={()=>setStatus("Delivered")}>Deliver</button>
       <h2>Start editing to see some magic happen!</h2>
     </div>
   );
